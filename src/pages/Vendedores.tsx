1671b61aade429f0dd5c6df115e7b77335b1e345
PedroHMFreire <pedrohfreire@gmail.com>
2025-08-02 09:41:43 -0300

    Update Vendedores.tsx

--- src/pages/Vendedores.tsx
+++ src/pages/Vendedores.tsx
@@ -121,7 +121,7 @@ const Vendedores = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 dark:bg-[#101624]">
       <Header />
       
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
